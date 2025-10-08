9a639a5e8983015cc4a4af8217e70d040f59dfcc
KaranRathod2003 <kr04391@gmail.com>
2025-10-08 21:44:24 +0530

    feat : Clean up the operation of UI

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -17,6 +17,9 @@ const Login = () => {
     const dummytoken = "abc123";
     login(dummyUser, dummytoken);
     console.log("User", dummyUser," ", " Token : ",  dummytoken);
+    setEmail("");
+    setName("");
+    setPasword("");
   }
 
   
@@ -37,16 +40,16 @@ const Login = () => {
               <form className="space-y-4 md:space-y-6" action="#">
                 <div>
                   <label for="text" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name</label>
-                  <input type="text" name="text" onChange={(e)=>setName(e.target.value)} id="text" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name" required="" />
+                  <input type="text" name="text" onChange={(e)=>setName(e.target.value)} value={name} id="text" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name" required="" />
                 </div>
                 <div>
                   <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                  <input type="email" name="email" onChange={(e)=>setEmail(e.target.value)} id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
+                  <input type="email" name="email" onChange={(e)=>setEmail(e.target.value)} value={email} id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
                 </div>
 
                 <div>
                   <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                  <input type="password" name="password" onChange={(e)=>setPasword(e.target.value)} id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                  <input type="password" name="password" onChange={(e)=>setPasword(e.target.value)} value={password} id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
                 </div>
                 <div className="flex items-center justify-between">
                   <div className="flex items-start cursor-pointer">
