0db784fc7c19e2cb2c7288cd0a0e38ad68fd9691
KaranRathod2003 <kr04391@gmail.com>
2025-10-16 13:10:09 +0530

    feat : backend environment established

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require ('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+
+
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send("server is running...");
+})
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`server is running on ${PORT}`));
