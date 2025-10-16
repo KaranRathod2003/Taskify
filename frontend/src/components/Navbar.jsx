ebbf1ba36207d63bdd1f640dce7154862286424d
KaranRathod2003 <kr04391@gmail.com>
2025-10-16 11:54:34 +0530

    feat : conditional rendering of User and admin dashboard

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import useAuth from '../context/AuthContext'
 
 const Navbar = () => {
+    const {isLoggedIn, user} = useAuth();
     return (
         <nav className='max-w-screen h-16 bg-gray-300'>
             <ul className='flex items-center justify-center gap-12 h-full'>
                 <li><Link to="/">Home</Link></li>
-                <li><Link to="/login">Login</Link></li>
-                <li><Link to="/register">Register</Link></li>
+                {!isLoggedIn && (          <>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/register">Register</Link></li>
+          </>)}
+          {
+            isLoggedIn && (
+                <>
                 <li><Link to="/dashboard">User Dashboard</Link></li>
-                <li><Link to="/admindashboard">Admin Dashboard</Link></li>
+                {user?.role === "admin" && (<li><Link to="/admindashboard">Admin Dashboard</Link></li>)}
+                </>
+            )
+          }
+                
+
             </ul>
         </nav>
     )
