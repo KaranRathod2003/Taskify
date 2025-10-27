072c6d54ad54af707d96b56160150776623f77fa
KaranRathod2003 <kr04391@gmail.com>
2025-10-27 13:20:50 +0530

    data model created - Task

---
+++ backend/models/tasks.models.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+import { type } from "requests";
+
+const taskSchema = mongoose.Schema({
+    title: {
+        type : String,
+        trim : true,
+        required : true,
+    },
+    description : {
+        type : String,
+        required : true,
+    },
+    createdBy : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        required : true
+    },
+    assignedTo : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        required : true
+    },
+    status : {
+        type : String,
+        enum : ["pending", "in-progress", "completed"],
+        default : "pending"
+    },
+    submissionDate: {
+      type: Date,
+    },
+    completedAt: {
+      type: Date,
+    },
+
+}, {timestamps : true});
+
+export const Task = mongoose.model("Task", taskSchema);
