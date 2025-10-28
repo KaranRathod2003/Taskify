8607ce632ed074e33b6c33f2fad5ad37e5d00270
KaranRathod2003 <kr04391@gmail.com>
2025-10-28 12:50:28 +0530

    MONGO DB CONNECTION

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,7 @@
-const express = require ('express');
 const dotenv = require('dotenv');
-
+const { default: connectDB } = require('./config/db');
 dotenv.config();
-const app = express();
 
 
-app.use(express.json());
-app.get('/', (req, res) => {
-    res.send("server is running...");
-})
+connectDB();
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`server is running on ${PORT}`));
