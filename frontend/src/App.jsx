b8e85f4948f8fcb02327113fabe1bdf72ef8bd16
KaranRathod2003 <kr04391@gmail.com>
2025-10-29 22:54:48 +0530

    page redirect bug fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,43 @@
-import { useState } from 'react'
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
-import RootLayout from './layout/RootLayout'
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
-import ProtectedRotes from './routes/ProtectedRotes';
-import AdminDashboard from './pages/AdminDashboard';
-import Unautharized from './pages/Unautharized';
-import RolesRoute from './routes/RolesRoute';
+  import { useState } from 'react'
+  import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+  import RootLayout from './layout/RootLayout'
+  import Home from "./pages/Home";
+  import Login from "./pages/Login";
+  import Register from "./pages/Register";
+  import Dashboard from "./pages/Dashboard";
+  import ProtectedRotes from './routes/ProtectedRotes';
+  import AdminDashboard from './pages/AdminDashboard';
+  import Unautharized from './pages/Unautharized';
+  import RolesRoute from './routes/RolesRoute';
 
 
-function App() {
-  const router = createBrowserRouter(createRoutesFromElements(<Route path='/' element={<RootLayout />} >
-    <Route index element={<Home />} /> {/* home page */}
-    <Route path="login" element={<Login />} />
-    <Route path="/unauthorized" element={<Unautharized />} />
-    <Route path="register" element={<Register />} />
-    <Route path="admindashboard" element={
-      <ProtectedRotes>
-        <RolesRoute allowedRoles={['admin']}>
-        <AdminDashboard />
-      </RolesRoute>
-      </ProtectedRotes>
-      
-    } />
-    <Route path="dashboard" element={<ProtectedRotes>
-      <RolesRoute allowedRoles={['user']}>
-        <Dashboard />
-      </RolesRoute>
-    </ProtectedRotes>} />
-  </Route>))
+  function App() {
+    const router = createBrowserRouter(createRoutesFromElements(<Route path='/' element={<RootLayout />} >
+      <Route index element={<Home />} /> {/* home page */}
+      <Route path="login" element={<Login />} />
+      <Route path="/unauthorized" element={<Unautharized />} />
+      <Route path="register" element={<Register />} />
+      <Route path="admindashboard" element={
+        <ProtectedRotes>
+          <RolesRoute allowedRoles={['admin']}>
+          <AdminDashboard />
+        </RolesRoute>
+        </ProtectedRotes>
+        
+      } />
+      <Route path="dashboard" element={<ProtectedRotes>
+        <RolesRoute allowedRoles={['user']}>
+          <Dashboard />
+        </RolesRoute>
+      </ProtectedRotes>} />
+    </Route>))
 
 
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  )
-}
+    return (
+      <>
+        <RouterProvider router={router} />
+      </>
+    )
+  }
 
-export default App
+  export default App
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import Button from '../components/Button'
 import useAuth from '../context/AuthContext'
 import API from '../api/axios';
@@ -16,8 +16,8 @@ const Login = () => {
 
 
 
-// form me
-{/* <div>
+  // form me
+  {/* <div>
   <label className="block mb-2">Role</label>
   <select value={role} onChange={(e) => setRole(e.target.value)} className="bg-gray-50 border rounded p-2 w-full">
     <option value="user">User</option>
@@ -33,14 +33,18 @@ const Login = () => {
     }
     setLoading(true);
     try {
-      const res = await API.post("/users/login", {email, password, role});
+      const res = await API.post("/users/login", { email, password, role });
 
-      const {user, accessToken} = res.data.data;
+      const { user, accessToken } = res.data.data;
       login(user, accessToken);
-      navigate("/")
+      if (user.role === "admin") {
+        navigate("/");
+      } else {
+        navigate("/");
+      }
     } catch (error) {
       alert(error.response?.data?.message || "Login failed");
-    }finally{
+    } finally {
       setLoading(false);
     }
 
@@ -55,68 +59,68 @@ const Login = () => {
 
   return (
     <>
-    <section className="bg-gray-50 dark:bg-gray-900">
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <h1 className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-          Taskify
-        </h1>
-        <div className="w-full bg-white rounded-lg shadow sm:max-w-md xl:p-0 dark:bg-gray-800">
-          <div className="p-6 space-y-6">
-            <h1 className="text-xl font-bold leading-tight text-gray-900 dark:text-white">
-              Login to your account
-            </h1>
+      <section className="bg-gray-50 dark:bg-gray-900">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <h1 className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+            Taskify
+          </h1>
+          <div className="w-full bg-white rounded-lg shadow sm:max-w-md xl:p-0 dark:bg-gray-800">
+            <div className="p-6 space-y-6">
+              <h1 className="text-xl font-bold leading-tight text-gray-900 dark:text-white">
+                Login to your account
+              </h1>
 
-            <form onSubmit={handleSubmit} className="space-y-4">
-              <div>
-                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  placeholder="name@company.com"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5"
+              <form onSubmit={handleSubmit} className="space-y-4">
+                <div>
+                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="name@company.com"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5"
+                  />
+                </div>
+
+                <div>
+                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="••••••••"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5"
+                  />
+                </div>
+
+                <div>
+                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    Role
+                  </label>
+                  <select
+                    value={role}
+                    onChange={(e) => setRole(e.target.value)}
+                    className="bg-gray-50 border rounded p-2 w-full"
+                  >
+                    <option value="user">User</option>
+                    <option value="admin">Admin</option>
+                  </select>
+                </div>
+
+                <Button
+                  title={loading ? "Logging in..." : "Login"}
+                  type="submit"
+                  disabled={loading}
                 />
-              </div>
-
-              <div>
-                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                  Password
-                </label>
-                <input
-                  type="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5"
-                />
-              </div>
-
-              <div>
-                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                  Role
-                </label>
-                <select
-                  value={role}
-                  onChange={(e) => setRole(e.target.value)}
-                  className="bg-gray-50 border rounded p-2 w-full"
-                >
-                  <option value="user">User</option>
-                  <option value="admin">Admin</option>
-                </select>
-              </div>
-
-              <Button
-                title={loading ? "Logging in..." : "Login"}
-                type="submit"
-                disabled={loading}
-              />
-            </form>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
     </>
   )
 }
